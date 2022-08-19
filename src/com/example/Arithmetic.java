cbc2578cee9c7184950f012dbefbdcfdc3024d05
fortunen <fortunezee@gmail.com>
2022-08-19 11:24:07 +0200

    'First commit'

---
+++ src/com/example/Arithmetic.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Arithmetic
+{
+    public static void main(String args[]){
+        int a=21;
+        int b=84;
+
+
+        //operator addition,subtraction,multiplication,division and percentage operators
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+       //expression operator with different arithmetic operators
+        int expression =10*10/5+3-1*4/2;
+        System.out.println("answer = "+expression);
+
+
+    }
+}
+
