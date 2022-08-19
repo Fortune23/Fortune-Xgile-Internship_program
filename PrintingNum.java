3530ec276d53ca9037b69f3a20ab8e26a4c4f6c7
Fortune23 <110975710+Fortune23@users.noreply.github.com>
2022-08-19 14:02:31 +0200

    Add files via upload

---
+++ PrintingNum.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class PrintingNum
+{
+    public static void main(String[] args)
+    {
+        int n=12;
+        if (n>10)
+        {
+            System.out.println("n is greater than 10");
+
+        }
+        else if(n==10)
+        {
+            System.out.println("n is equal than 12");
+        }
+        else
+        {
+            System.out.println("n is less than");
+        }
+    }
+}
